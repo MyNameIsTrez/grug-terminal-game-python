a009299ef61d43c1954fe5d0816746f82bd9b451
MyNameIsTrez <welfje@gmail.com>
2024-09-26 20:09:56 +0200

    Rename variables

--- demo.c
+++ demo.c
@@ -4,22 +4,22 @@
 #include <assert.h>
 
 int bar(float f) {
-	PyObject *myModule = PyImport_ImportModule("main");
-	assert(myModule);
+	PyObject *module = PyImport_ImportModule("main");
+	assert(module);
 
-	PyObject *myFunction = PyObject_GetAttrString(myModule, "bar");
-	assert(myFunction);
+	PyObject *fn = PyObject_GetAttrString(module, "bar");
+	assert(fn);
 
 	PyObject *args = PyTuple_Pack(0);
 	assert(args);
 
-	PyObject *myResult = PyObject_CallObject(myFunction, args);
-	assert(myResult);
+	PyObject *result = PyObject_CallObject(fn, args);
+	assert(result);
 
 	// In newer versions of Python, this function is identical,
 	// but doesn't start with an underscore:
 	// https://github.com/python/cpython/commit/be436e08b8bd9fcd2202d6ce4d924bba7551e96f
-	return f + _PyLong_AsInt(myResult);
+	return f + _PyLong_AsInt(result);
 }
 
 int foo(float f) {
