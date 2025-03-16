543ae050b8721534033292a49d7929ed38c2a132
MyNameIsTrez <welfje@gmail.com>
2025-03-16 21:20:38 +0100

    grug removed the concept of a define() function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import random
 import sys
 import time
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, replace
 from enum import Enum, auto
 
 
@@ -19,11 +19,11 @@ class GrugError(ctypes.Structure):
 class GrugFile(ctypes.Structure):
     _fields_ = [
         ("name", ctypes.c_char_p),
+        ("entity", ctypes.c_char_p),
+        ("entity_type", ctypes.c_char_p),
         ("dll", ctypes.c_void_p),
-        ("define_fn", ctypes.PYFUNCTYPE(None)),  # The None means it returns void
         ("globals_size", ctypes.c_size_t),
         ("init_globals_fn", ctypes.PYFUNCTYPE(None, ctypes.c_void_p, ctypes.c_uint64)),
-        ("define_type", ctypes.c_char_p),
         ("on_fns", ctypes.c_void_p),
         ("resource_mtimes", ctypes.POINTER(ctypes.c_int64)),
     ]
@@ -54,23 +54,33 @@ class GrugModified(ctypes.Structure):
     ]
 
 
+class HumanOnFns(ctypes.Structure):
+    _fields_ = [
+        ("spawn", ctypes.PYFUNCTYPE(None, ctypes.c_void_p)),
+        ("despawn", ctypes.PYFUNCTYPE(None, ctypes.c_void_p)),
+    ]
+
+
 class ToolOnFns(ctypes.Structure):
     _fields_ = [
+        ("spawn", ctypes.PYFUNCTYPE(None, ctypes.c_void_p)),
+        ("despawn", ctypes.PYFUNCTYPE(None, ctypes.c_void_p)),
         ("use", ctypes.PYFUNCTYPE(None, ctypes.c_void_p)),
     ]
 
 
 @dataclass
 class Human:
-    name: str
-    health: int
-    buy_gold_value: int
-    kill_gold_value: int
+    name: str = ""
+    health: int = -1
+    buy_gold_value: int = -1
+    kill_gold_value: int = -1
 
     # These are not initialized by mods
     id: int = -1
     opponent_id: int = -1
     max_health: int = -1
+    on_fns: HumanOnFns = None
 
 
 @dataclass
@@ -112,7 +122,7 @@ PLAYER_INDEX = 0
 OPPONENT_INDEX = 1
 
 data = Data(
-    humans=[None, None],
+    humans=[Human(), Human()],
     human_dlls=[None, None],
     human_globals=[None, None],
     tools=[Tool(), Tool()],
@@ -120,8 +130,8 @@ data = Data(
     tool_globals=[None, None],
 )
 
-human_definition: Human = None
-tool_definition: Tool = None
+human_on_spawn_data = Human()
+tool_on_spawn_data = Tool()
 grug_dll = None
 on_fn_name = None
 on_fn_path = None
@@ -171,44 +181,137 @@ def game_fn_get_human_parent(tool_id):
     return data.tools[tool_id].human_parent_id
 
 
-def game_fn_define_human(name, health, buy_gold_value, kill_gold_value):
-    global human_definition
-    human_definition = Human(name, health, buy_gold_value, kill_gold_value)
+def game_fn_print_string(msg):
+    print(msg)
 
 
-def game_fn_define_tool(name, buy_gold_value):
-    global tool_definition
-    tool_definition = Tool(name, buy_gold_value)
+set_human_kill_gold_value_called = False
+set_human_buy_gold_value_called = False
+set_human_health_called = False
+set_human_name_called = False
+
+set_tool_buy_gold_value_called = False
+set_tool_name_called = False
 
 
-def get_type_files_impl(dir: GrugModDir, define_type):
+def game_fn_set_human_kill_gold_value(kill_gold_value):
+    global set_human_kill_gold_value_called
+    if set_human_kill_gold_value_called:
+        print(
+            f"set_human_kill_gold_value() was called twice by on_spawn()",
+            file=sys.stderr,
+        )
+        return
+    set_human_kill_gold_value_called = True
+
+    human_on_spawn_data.kill_gold_value = kill_gold_value
+
+
+def game_fn_set_human_buy_gold_value(buy_gold_value):
+    global set_human_buy_gold_value_called
+    if set_human_buy_gold_value_called:
+        print(
+            f"set_human_buy_gold_value() was called twice by on_spawn()",
+            file=sys.stderr,
+        )
+        return
+    set_human_buy_gold_value_called = True
+
+    human_on_spawn_data.buy_gold_value = buy_gold_value
+
+
+def game_fn_set_human_health(health):
+    global set_human_health_called
+    if set_human_health_called:
+        print(
+            f"set_human_health() was called twice by on_spawn()",
+            file=sys.stderr,
+        )
+        return
+    set_human_health_called = True
+
+    human_on_spawn_data.health = health
+
+
+def game_fn_set_human_name(name):
+    global set_human_name_called
+    if set_human_name_called:
+        print(
+            f"set_human_name() was called twice by on_spawn()",
+            file=sys.stderr,
+        )
+        return
+    set_human_name_called = True
+
+    human_on_spawn_data.name = name
+
+
+def game_fn_set_tool_buy_gold_value(buy_gold_value):
+    global set_tool_buy_gold_value_called
+    if set_tool_buy_gold_value_called:
+        print(
+            f"set_tool_buy_gold_value() was called twice by on_spawn()",
+            file=sys.stderr,
+        )
+        return
+    set_tool_buy_gold_value_called = True
+
+    tool_on_spawn_data.buy_gold_value = buy_gold_value
+
+
+def game_fn_set_tool_name(name):
+    global set_tool_name_called
+    if set_tool_name_called:
+        print(
+            f"set_tool_name() was called twice by on_spawn()",
+            file=sys.stderr,
+        )
+        return
+    set_tool_name_called = True
+
+    tool_on_spawn_data.name = name
+
+
+def get_type_files_impl(dir: GrugModDir, entity_type):
     for i in range(dir.dirs_size):
-        get_type_files_impl(dir.dirs[i], define_type)
+        get_type_files_impl(dir.dirs[i], entity_type)
 
     for i in range(dir.files_size):
-        if dir.files[i].define_type.decode() == define_type:
+        if dir.files[i].entity_type.decode() == entity_type:
             data.type_files.append(dir.files[i])
 
 
-def get_type_files(define_type):
+def get_type_files(entity_type):
     data.type_files = []
 
     grug_mods = GrugModDir.in_dll(grug_dll, "grug_mods")
 
-    get_type_files_impl(grug_mods, define_type)
+    get_type_files_impl(grug_mods, entity_type)
 
     return data.type_files
+
+
+def call_human_on_despawn(on_fns, globals):
+    if on_fns.despawn:
+        on_fns.despawn(globals)
+
+
+def call_tool_on_despawn(on_fns, globals):
+    if on_fns.despawn:
+        on_fns.despawn(globals)
 
 
 def fight():
     player = data.humans[PLAYER_INDEX]
     opponent = data.humans[OPPONENT_INDEX]
 
-    player_tool_globals = data.tool_globals[PLAYER_INDEX]
-    opponent_tool_globals = data.tool_globals[OPPONENT_INDEX]
-
     player_tool = data.tools[PLAYER_INDEX]
     opponent_tool = data.tools[OPPONENT_INDEX]
+
+    opponent_human_globals = data.human_globals[OPPONENT_INDEX]
+
+    player_tool_globals = data.tool_globals[PLAYER_INDEX]
+    opponent_tool_globals = data.tool_globals[OPPONENT_INDEX]
 
     print(f"You have {player.health} health")
     print(f"The opponent has {opponent.health} health\n")
@@ -224,6 +327,7 @@ def fight():
 
     if opponent.health <= 0:
         print("The opponent died!")
+        call_human_on_despawn(opponent.on_fns, opponent_human_globals)
         time.sleep(1)
         data.state = State.PICKING_PLAYER
         data.gold += opponent.kill_gold_value
@@ -269,23 +373,109 @@ def read_size(prompt):
     return age
 
 
-def print_opponent_humans(files_defining_human):
-    global human_definition
+def call_human_on_spawn(entity, on_fns, globals):
+    global set_human_name_called
+    global set_human_health_called
+    global set_human_buy_gold_value_called
+    global set_human_kill_gold_value_called
 
-    for i, file in enumerate(files_defining_human):
-        file.define_fn()
-        human = human_definition
+    if not on_fns.spawn:
+        print(f"{entity.decode()} is missing on_spawn()", file=sys.stderr)
+        return True
+
+    set_human_name_called = False
+    set_human_health_called = False
+    set_human_buy_gold_value_called = False
+    set_human_kill_gold_value_called = False
+
+    on_fns.spawn(globals)
+
+    if not set_human_name_called:
+        print(
+            f"{entity.decode()} its on_spawn() did not call set_human_name()",
+            file=sys.stderr,
+        )
+        return True
+    if not set_human_health_called:
+        print(
+            f"{entity.decode()} its on_spawn() did not call set_human_health()",
+            file=sys.stderr,
+        )
+        return True
+    if not set_human_buy_gold_value_called:
+        print(
+            f"{entity.decode()} its on_spawn() did not call set_human_buy_gold_value()",
+            file=sys.stderr,
+        )
+        return True
+    if not set_human_kill_gold_value_called:
+        print(
+            f"{entity.decode()} its on_spawn() did not call set_human_kill_gold_value()",
+            file=sys.stderr,
+        )
+        return True
+
+    return False
+
+
+def call_tool_on_spawn(entity, on_fns, globals):
+    global set_tool_name_called
+    global set_tool_buy_gold_value_called
+
+    if not on_fns.spawn:
+        print(f"{entity.decode()} is missing on_spawn()", file=sys.stderr)
+        return True
+
+    set_tool_name_called = False
+    set_tool_buy_gold_value_called = False
+
+    on_fns.spawn(globals)
+
+    if not set_tool_name_called:
+        print(
+            f"{entity.decode()} its on_spawn() did not call set_tool_name()",
+            file=sys.stderr,
+        )
+        return True
+    if not set_tool_buy_gold_value_called:
+        print(
+            f"{entity.decode()} its on_spawn() did not call set_tool_buy_gold_value()",
+            file=sys.stderr,
+        )
+        return True
+
+    return False
+
+
+def print_opponent_humans(human_files):
+    global human_on_spawn_data
+
+    for i, file in enumerate(human_files):
+        globals = (ctypes.c_byte * file.globals_size)()
+        file.init_globals_fn(globals, 0)
+
+        on_fns = HumanOnFns.from_address(file.on_fns)
+
+        if call_human_on_spawn(file.entity, on_fns, globals):
+            return True
+
+        human = human_on_spawn_data
+
         print(f"{i + 1}. {human.name}, worth {human.kill_gold_value} gold when killed")
 
+        call_human_on_despawn(on_fns, globals)
+
     print("")
+    return False
 
 
 def pick_opponent():
     print(f"You have {data.gold} gold\n")
 
-    files_defining_human = get_type_files("human")
+    human_files = get_type_files("human")
 
-    print_opponent_humans(files_defining_human)
+    if print_opponent_humans(human_files):
+        return
 
     opponent_number = read_size(
         f"Type the number next to the human you want to fight:\n"
@@ -297,19 +487,28 @@ def pick_opponent():
         print("The minimum number you can enter is 1", file=sys.stderr)
         return
 
-    if opponent_number > len(files_defining_human):
+    if opponent_number > len(human_files):
         print(
-            f"The maximum number you can enter is {len(files_defining_human)}",
+            f"The maximum number you can enter is {len(human_files)}",
             file=sys.stderr,
         )
         return
 
     opponent_index = opponent_number - 1
 
-    file = files_defining_human[opponent_index]
+    file = human_files[opponent_index]
 
-    file.define_fn()
-    human = human_definition
+    data.human_globals[OPPONENT_INDEX] = (ctypes.c_byte * file.globals_size)()
+    file.init_globals_fn(data.human_globals[OPPONENT_INDEX], OPPONENT_INDEX)
+
+    on_fns = HumanOnFns.from_address(file.on_fns)
+
+    if call_human_on_spawn(file.entity, on_fns, data.human_globals[OPPONENT_INDEX]):
+        return
+
+    human = replace(human_on_spawn_data)
+
+    human.on_fns = on_fns
 
     human.id = OPPONENT_INDEX
     human.opponent_id = PLAYER_INDEX
@@ -319,48 +518,61 @@ def pick_opponent():
     data.humans[OPPONENT_INDEX] = human
     data.human_dlls[OPPONENT_INDEX] = file.dll
 
-    data.human_globals[OPPONENT_INDEX] = (ctypes.c_byte * file.globals_size)()
-    file.init_globals_fn(data.human_globals[OPPONENT_INDEX], OPPONENT_INDEX)
-
     # Give the opponent a random tool
-    files_defining_tool = get_type_files("tool")
-    tool_index = random.randrange(len(files_defining_tool))
+    tool_files = get_type_files("tool")
+    tool_index = random.randrange(len(tool_files))
 
-    file = files_defining_tool[tool_index]
+    file = tool_files[tool_index]
 
-    file.define_fn()
-    tool = tool_definition
+    data.tool_globals[OPPONENT_INDEX] = (ctypes.c_byte * file.globals_size)()
+    file.init_globals_fn(data.tool_globals[OPPONENT_INDEX], OPPONENT_INDEX)
 
-    tool.on_fns = ToolOnFns.from_address(file.on_fns)
+    on_fns = ToolOnFns.from_address(file.on_fns)
+
+    if call_tool_on_spawn(file.entity, on_fns, data.tool_globals[OPPONENT_INDEX]):
+        return
+
+    tool = replace(tool_on_spawn_data)
+
+    tool.on_fns = on_fns
 
     tool.human_parent_id = OPPONENT_INDEX
 
     data.tools[OPPONENT_INDEX] = tool
     data.tool_dlls[OPPONENT_INDEX] = file.dll
 
-    data.tool_globals[OPPONENT_INDEX] = (ctypes.c_byte * file.globals_size)()
-    file.init_globals_fn(data.tool_globals[OPPONENT_INDEX], OPPONENT_INDEX)
-
     data.state = State.FIGHTING
 
 
-def print_tools(files_defining_tool):
-    global tool_definition
+def print_tools(tool_files):
+    global tool_on_spawn_data
 
-    for i, file in enumerate(files_defining_tool):
-        file.define_fn()
-        tool = tool_definition
+    for i, file in enumerate(tool_files):
+        globals = (ctypes.c_byte * file.globals_size)()
+        file.init_globals_fn(globals, 0)
+
+        on_fns = ToolOnFns.from_address(file.on_fns)
+
+        if call_tool_on_spawn(file.entity, on_fns, globals):
+            return True
+
+        tool = tool_on_spawn_data
+
         print(f"{i + 1}. {tool.name}, costing {tool.buy_gold_value} gold")
 
+        call_tool_on_despawn(on_fns, globals)
+
     print("")
+    return False
 
 
 def pick_tools():
     print(f"You have {data.gold} gold\n")
 
-    files_defining_tool = get_type_files("tool")
+    tool_files = get_type_files("tool")
 
-    print_tools(files_defining_tool)
+    if print_tools(tool_files):
+        return
 
     tool_number = read_size(
         f"Type the number next to the tool you want to buy{" (type 0 to skip)" if data.player_has_tool else ""}:\n"
@@ -376,56 +588,78 @@ def pick_tools():
         print("The minimum number you can enter is 1", file=sys.stderr)
         return
 
-    if tool_number > len(files_defining_tool):
+    if tool_number > len(tool_files):
         print(
-            f"The maximum number you can enter is {len(files_defining_tool)}",
+            f"The maximum number you can enter is {len(tool_files)}",
             file=sys.stderr,
         )
         return
 
+    if data.tools[PLAYER_INDEX].on_fns:
+        call_tool_on_despawn(
+            data.tools[PLAYER_INDEX].on_fns, data.tool_globals[PLAYER_INDEX]
+        )
+
     tool_index = tool_number - 1
 
-    file = files_defining_tool[tool_index]
+    file = tool_files[tool_index]
 
-    file.define_fn()
-    tool = tool_definition
+    data.tool_globals[PLAYER_INDEX] = (ctypes.c_byte * file.globals_size)()
+    file.init_globals_fn(data.tool_globals[PLAYER_INDEX], PLAYER_INDEX)
 
-    tool.on_fns = ToolOnFns.from_address(file.on_fns)
+    on_fns = ToolOnFns.from_address(file.on_fns)
+
+    if call_tool_on_spawn(file.entity, on_fns, data.tool_globals[PLAYER_INDEX]):
+        return
+
+    tool = replace(tool_on_spawn_data)
 
     if tool.buy_gold_value > data.gold:
         print("You don't have enough gold to pick that tool", file=sys.stderr)
         return
-
     data.gold -= tool.buy_gold_value
+
+    tool.on_fns = on_fns
 
     tool.human_parent_id = PLAYER_INDEX
 
     data.tools[PLAYER_INDEX] = tool
     data.tool_dlls[PLAYER_INDEX] = file.dll
 
-    data.tool_globals[PLAYER_INDEX] = (ctypes.c_byte * file.globals_size)()
-    file.init_globals_fn(data.tool_globals[PLAYER_INDEX], PLAYER_INDEX)
-
     data.player_has_tool = True
 
+    data.state = State.PICKING_OPPONENT
 
-def print_playable_humans(files_defining_human):
-    global human_definition
 
-    for i, file in enumerate(files_defining_human):
-        file.define_fn()
-        human = human_definition
+def print_playable_humans(human_files):
+    global human_on_spawn_data
+
+    for i, file in enumerate(human_files):
+        globals = (ctypes.c_byte * file.globals_size)()
+        file.init_globals_fn(globals, 0)
+
+        on_fns = HumanOnFns.from_address(file.on_fns)
+
+        if call_human_on_spawn(file.entity, on_fns, globals):
+            return True
+
+        human = human_on_spawn_data
+
         print(f"{i + 1}. {human.name}, costing {human.buy_gold_value} gold")
 
+        call_human_on_despawn(on_fns, globals)
+
     print("")
+    return False
 
 
 def pick_player():
     print(f"You have {data.gold} gold\n")
 
-    files_defining_human = get_type_files("human")
+    human_files = get_type_files("human")
 
-    print_playable_humans(files_defining_human)
+    if print_playable_humans(human_files):
+        return
 
     player_number = read_size(
         f"Type the number next to the human you want to play as{" (type 0 to skip)" if data.player_has_human else ""}:\n"
@@ -441,25 +675,38 @@ def pick_player():
         print("The minimum number you can enter is 1", file=sys.stderr)
         return
 
-    if player_number > len(files_defining_human):
+    if player_number > len(human_files):
         print(
-            f"The maximum number you can enter is {len(files_defining_human)}",
+            f"The maximum number you can enter is {len(human_files)}",
             file=sys.stderr,
         )
         return
 
+    if data.humans[PLAYER_INDEX].on_fns:
+        call_human_on_despawn(
+            data.humans[PLAYER_INDEX].on_fns, data.human_globals[PLAYER_INDEX]
+        )
+
     player_index = player_number - 1
 
-    file = files_defining_human[player_index]
+    file = human_files[player_index]
 
-    file.define_fn()
-    human = human_definition
+    data.human_globals[PLAYER_INDEX] = (ctypes.c_byte * file.globals_size)()
+    file.init_globals_fn(data.human_globals[PLAYER_INDEX], PLAYER_INDEX)
+
+    on_fns = HumanOnFns.from_address(file.on_fns)
+
+    if call_human_on_spawn(file.entity, on_fns, data.human_globals[PLAYER_INDEX]):
+        return
+
+    human = replace(human_on_spawn_data)
 
     if human.buy_gold_value > data.gold:
         print("You don't have enough gold to pick that human", file=sys.stderr)
         return
-
     data.gold -= human.buy_gold_value
+
+    human.on_fns = on_fns
 
     human.id = PLAYER_INDEX
     human.opponent_id = OPPONENT_INDEX
@@ -468,9 +715,6 @@ def pick_player():
 
     data.humans[PLAYER_INDEX] = human
     data.human_dlls[PLAYER_INDEX] = file.dll
-
-    data.human_globals[PLAYER_INDEX] = (ctypes.c_byte * file.globals_size)()
-    file.init_globals_fn(data.human_globals[PLAYER_INDEX], PLAYER_INDEX)
 
     data.player_has_human = True
 
@@ -505,6 +749,10 @@ def reload_modified_entities():
 
                 data.human_globals[i] = (ctypes.c_byte * file.globals_size)()
                 file.init_globals_fn(data.human_globals[i], i)
+
+                data.humans[i].on_fns = (
+                    HumanOnFns.from_address(file.on_fns) if file.on_fns else None
+                )
 
         for i in range(2):
             if reload.old_dll == data.tool_dlls[i]:
